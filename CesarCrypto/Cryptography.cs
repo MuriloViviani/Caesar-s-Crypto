e226b5745f838e53c936fc02f07aec69bc4932d7
murilo viviani <muriloviviani@hotmail.com>
2017-09-28 21:43:58 -0300

    Add project files.

---
+++ CesarCrypto/Cryptography.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCrypto
+{
+    public class Cryptography
+    {
+        private static List<string> letters = new List<string>()
+                { "a", "b", "c", "d", "e", "f", "g", "h",
+                  "i", "j", "k", "l", "m", "n", "o", "p",
+                  "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+        /// <summary>
+        /// Cryptograph an word or phrase
+        /// </summary>
+        /// <param name="phrase">Phrase to be cryptographed</param>
+        /// <param name="key">Key</param>
+        /// <returns>Crypted Word or phrase</returns>
+        public string Encrypt(string phrase, int key)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var letter in phrase)
+            {
+                // Search in the dictionary
+                var x = letters.BinarySearch(letter.ToString().ToLower());
+
+                if (x < 0) // Charactere not found
+                    sb.Append(letter.ToString());
+                else
+                {
+                    if ((x + key) >= 26)
+                        x -= 26;
+
+                    if (string.Equals(letter.ToString(), letter.ToString().ToUpper()))
+                        sb.Append(letters[x + key].ToUpper());
+                    else
+                        sb.Append(letters[x + key]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Decryptograph an word or phrase
+        /// </summary>
+        /// <param name="phrase"></param>
+        /// <param name="key"></param>
+        /// <returns>Decrypted Word or phrase</returns>
+        public string Decrypt(string phrase, int key)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var letter in phrase)
+            {
+                var x = letters.BinarySearch(letter.ToString().ToLower());
+
+                if (x < 0) // Charactere not found
+                    sb.Append(letter.ToString());
+                else
+                {
+                    if ((x - key) < 0)
+                        x += 26;
+
+                    if (string.Equals(letter.ToString(), letter.ToString().ToUpper()))
+                        sb.Append(letters[x - key].ToUpper());
+                    else
+                        sb.Append(letters[x - key]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CesarCrypto/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CaesarCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+   _____         _____ 
+  / ____|       / ____|
+ | |           | |     
+ | |           | |     
+ | |___        | |____ 
+  \_____AESAR'S \_____|ODE
+               
+");
+            Console.WriteLine("=================================");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("\t1 - Encrypt");
+            Console.WriteLine("\t2 - Decrypt");
+            Console.WriteLine("\t0 - Exit");
+            Console.WriteLine("=================================");
+
+            int op = -1;
+            do
+            {
+                Console.WriteLine("What do you wanna do?");
+                op = int.Parse(Console.ReadLine());
+            } while (op < 0 || op > 2);
+
+            string phrase = "", cryptoPhrase = "";
+            int key = 0;
+            Cryptography crypt = new Cryptography();
+            switch (op)
+            {
+                case 1: // Encrypt
+                    Console.WriteLine("What do you want to Encrypt?");
+                    phrase = Console.ReadLine();
+
+                    Console.WriteLine("What's the code? (a number from 0 to 25)");
+                    key = int.Parse(Console.ReadLine());
+
+                    cryptoPhrase = crypt.Encrypt(phrase, key);
+                    Console.WriteLine("\nYour Encrypted Phrase is:");
+                    Console.WriteLine("\t\"" + cryptoPhrase + "\", Key - " + key);
+                    break;
+
+                case 2: // Decryptograph
+                    Console.WriteLine("What do you want to Decrypt?");
+                    phrase = Console.ReadLine();
+
+                    Console.WriteLine("What's the code? (a number from 0 to 25)");
+                    key = int.Parse(Console.ReadLine());
+
+                    cryptoPhrase = crypt.Decrypt(phrase, key);
+                    Console.WriteLine("\nYour Decrypted Phrase is:");
+                    Console.WriteLine("\t\"" + cryptoPhrase + "\", Key - " + key);
+                    break;
+
+                default: // Exit
+                    Console.WriteLine("\nFinishing Application...\n\nThanks for using it!\n");
+
+                    Console.WriteLine("Press enter to continue...");
+                    Console.Read();
+
+                    Environment.Exit(0);
+                    break;
+            }
+
+            Console.WriteLine("\nFinishing Application...\n\nThanks for using it!\n");
+
+            Console.WriteLine("Press enter to continue...");
+            Console.Read();
+
+            Environment.Exit(0);
+        }
+    }
+}
